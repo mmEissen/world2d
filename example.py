8024c0b1f9df837adad43f529080d8edd8660cb1
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2017-09-06 00:29:15 +0100

    set up world_painter

--- example.py
+++ example.py
@@ -5,10 +5,21 @@ from PyQt5.QtWidgets import QApplication
 
 from widgets import World2D
 
+
+class ExampleWorld(World2D):
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+
+        painter = self.world_painter()
+        painter.drawRect(0, 0, 100, 100)
+        painter.end()
+
+
 def main():
     app = QApplication(sys.argv)
 
-    w = World2D()
+    w = ExampleWorld()
     w.resize(800, 600)
     w.move(100, 100)
     w.setWindowTitle('Simple')
--- widgets.py
+++ widgets.py
@@ -1,6 +1,6 @@
 
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtGui import QTransform
+from PyQt5.QtGui import QTransform, QPainter
 
 class World2D(QWidget):
     """A Widget which has it's own 2D world space, onto which shapes can be drawn.
@@ -8,3 +8,25 @@ class World2D(QWidget):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.world_transform = QTransform()
+        self._is_mouse_down = False
+
+    def inverse_world_transform(self):
+        return self.world_transform()
+
+    def world_painter(self):
+        painter = QPainter()
+        painter.begin(self)
+        painter.setWorldMatrixEnabled(True)
+        painter.setWorldTransform(self.world_transform)
+
+        return painter
+
+    def mousePressEvent(self, mouse_event):
+        super().mousePressEvent(mouse_event)
+
+    def mouseReleaseEvent(self, mouse_event):
+        super().mouseReleaseEvent(mouse_event)
+
+    def mouseMoveEvent(self, mouse_event):
+        super().mouseMoveEvent(mouse_event)
